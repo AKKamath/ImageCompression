020203941a61b7d140d62a116a1ea09ea8d0b1ee
AdityaK <a_kamath@hotmail.com>
2017-08-07 19:54:16 +0530

    Improve the calculation of node sizes. Also allow user to select image

--- ImageComp.py
+++ ImageComp.py
@@ -7,16 +7,17 @@ class Point(object):
 		self.x = x
 		self.y = y
 
-
+# Keeps track of RGB values of a pixel
 class Pixel(object):
-	def __init__(self, color = [0, 0, 0], topLeft = Point(0, 0), bottomRight = Point(0, 0)):
+	def __init__(self, color = [-1, -1, -1], topLeft = Point(0, 0), bottomRight = Point(0, 0)):
 		self.R = color[0]
 		self.G = color[1]
 		self.B = color[2]
 		self.topLeft = topLeft
 		self.bottomRight = bottomRight
 
-class quadtree():
+# The main class
+class QuadTree():
 	def __init__(self, image):
 		# Total number of nodes of tree
 		self.size = 0
@@ -36,26 +37,59 @@ class quadtree():
 		# Initialize array elements
 		for i in range(0, self.size):
 			self.tree.append(Pixel())
-		# Place leaf nodes into array
-		count = 0
-		for i in range(image.size[0] - 1, 0, -2):
-			for j in range(image.size[1] - 1, 0, -2):
-				self.tree[self.size - 1 - 4 * count] = Pixel(self.image[i, j], Point(i, j), Point(i, j))
-				self.tree[self.size - 2 - 4 * count] = Pixel(self.image[i, j - 1], Point(i, j - 1), Point(i, j - 1))
-				self.tree[self.size - 3 - 4 * count] = Pixel(self.image[i - 1, j], Point(i - 1, j), Point(i - 1, j))
-				self.tree[self.size - 4 - 4 * count] = Pixel(self.image[i - 1, j - 1], Point(i - 1, j - 1), Point(i - 1, j - 1))
-				count += 1
-		# Calculate and create parent nodes
-		for i in range(self.size - 4 * count - 1, -1, -1):
-			self.tree[i] = Pixel(
-				[(self.tree[4 * i + 1].R + self.tree[4 * i + 2].R + self.tree[4 * i + 3].R + self.tree[4 * i + 4].R) / 4,
-				 (self.tree[4 * i + 1].G + self.tree[4 * i + 2].G + self.tree[4 * i + 3].G + self.tree[4 * i + 4].G) / 4,
-				 (self.tree[4 * i + 1].B + self.tree[4 * i + 2].B + self.tree[4 * i + 3].B + self.tree[4 * i + 4].B) / 4],
-				 self.tree[4 * i + 1].topLeft, self.tree[4 * i + 4].bottomRight)
 		f = open("File.txt", "w")
-		for i in range(0, self.size):
-			f.write(str(i) + " " + str(self.tree[i].topLeft.x) + " " + str(self.tree[i].topLeft.y) + " " + str(self.tree[i].bottomRight.x) + " " + str(self.tree[i].bottomRight.y) + "\n")
+		f.write("\n")
+		f.close()
+		# Place nodes into array
+		self.createNode(0, Point(0, 0), Point(self.x - 1, self.y - 1))
+		
+	def createNode(self, pos, topLeft, bottomRight):
+		f = open("File.txt", "a")
+		f.write(str(pos) + " " + str(topLeft.x) + " " + str(topLeft.y) + " " + str(bottomRight.x) + " " + str(bottomRight.y) + "\n")
+		f.close()
+		# Make sure node should exist
+		if(pos >= self.size):
+			return
+		
+		# Check if node is a leaf node
+		if(topLeft.x == bottomRight.x and topLeft.y == bottomRight.y):
+			self.tree[pos] = Pixel(self.image[topLeft.x, topLeft.y], topLeft, bottomRight)
+			return
 
+		# Parent of leaf nodes
+		if(abs(topLeft.x - bottomRight.x) <= 1 and abs(topLeft.y - bottomRight.y) <= 1):
+			mid = Point(topLeft.x + 1, topLeft.y)
+			bottomMid = Point(topLeft.x, topLeft.y + 1)
+			self.createNode(pos * 4 + 1, topLeft, topLeft)
+			self.createNode(pos * 4 + 2, mid, mid)
+			self.createNode(pos * 4 + 3, bottomMid, bottomMid)
+			self.createNode(pos * 4 + 4, bottomRight, bottomRight)
+		# Other nodes	
+		else:		
+			# Store relevant positions
+			mid = Point((topLeft.x + bottomRight.x) / 2, (topLeft.y + bottomRight.y) / 2)
+			topMid = Point((topLeft.x + bottomRight.x) / 2, topLeft.y)
+			leftMid = Point(topLeft.x, (topLeft.y + bottomRight.y) / 2)
+			rightMid = Point(bottomRight.x, (topLeft.y + bottomRight.y) / 2)
+			bottomMid = Point((topLeft.x + bottomRight.x) / 2, bottomRight.y)
+
+			# Create children of current node
+			self.createNode(pos * 4 + 1, topLeft, mid)
+			self.createNode(pos * 4 + 2, Point(topMid.x + 1, topMid.y), rightMid)
+			self.createNode(pos * 4 + 3, Point(leftMid.x, leftMid.y + 1), bottomMid)
+			self.createNode(pos * 4 + 4, Point(mid.x + 1, mid.y + 1), bottomRight)
+
+		# Store requisite color information
+		R = (self.tree[pos * 4 + 1].R + self.tree[pos * 4 + 2].R + 
+			self.tree[pos * 4 + 3].R + self.tree[pos * 4 + 4].R) / 4
+		G = (self.tree[pos * 4 + 1].G + self.tree[pos * 4 + 2].G + 
+			self.tree[pos * 4 + 3].G + self.tree[pos * 4 + 4].G) / 4
+		B = (self.tree[pos * 4 + 1].B + self.tree[pos * 4 + 2].B + 
+			self.tree[pos * 4 + 3].B + self.tree[pos * 4 + 4].B) / 4
+		
+		# Update current node with values
+		self.tree[pos] = Pixel([R, G, B], topLeft, bottomRight)
+		
 	def disp(self, level):
 		start = 0
 		# Calculate position of starting node of given height
@@ -79,8 +113,8 @@ class quadtree():
 					pixels[x,y] = (i.R, i.G, i.B)
         # Display image
 		img.show()
-		
-img = Image.open("Image.ico").convert("RGB")
-tree = quadtree(img)
+img_name = raw_input("Enter name of image\n")
+img = Image.open(img_name).convert("RGB")
+tree = QuadTree(img)
 A = input("Input tree level\n")
 tree.disp(A)
